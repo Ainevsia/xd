f9d38501aa6b6b1a4586fb8a7c6d633e1ad81b5b
Ainevsia <zhipengxu@sjtu.edu.cn>
2020-01-21 16:05:13 +0800

    refactor finish

--- src/lib.rs
+++ src/lib.rs
@@ -37,3 +37,44 @@ pub fn get_content(file_name: &path::Path, buf: &mut Vec<u8>) -> Result<usize, E
         ).read_to_end(buf)?
     )
 }
+
+pub fn print_hex(buf: &Vec<u8>) {
+    let (mut cnt, mut idx) = (0, 0);
+    let mut line = [0u8; 16];
+
+    for &byte in buf.iter() {
+        if cnt == 16 {  // one line of hex end, print corresponding chars
+            print!("    ");
+            for &i in line.iter() {
+                if i >= 0x20 && i <= 0x7e { print!("{}", i as char) }
+                else { print!(".") }
+            }
+            print!("\n");
+            cnt = 0;
+        } else if cnt == 8 {    // middle of the hex, add one blank space
+            print!(" ");
+        }
+        if cnt == 0 {   // start a new line
+            print!("{:08x}    ", idx);
+            idx += 16;
+        }
+        line[cnt] = byte;
+        print!(" {:02x}", byte);
+        cnt += 1;
+    }
+
+    // print the remaing chars
+    if cnt < 8 { print!(" ") }
+    for _ in 0..16 - cnt { print!("   ") }
+    print!("    ");
+    for i in 0..cnt {
+        let c = line[i];
+        if c >= 0x20 && c <= 0x7e { print!("{}", c as char) }
+        else { print!(".") }
+    }
+
+    // finish summary
+    let length = idx - 16 + cnt;
+    println!("\n{:08x}", length);
+    println!("total length : {:#?} bytes", length);
+}
--- src/main.rs
+++ src/main.rs
@@ -7,48 +7,10 @@ fn main() -> Result<(), ExitFailure> {
     let file_name = opt.get_file_name();
     println!("File name : {:#?}", file_name);
 
+    // get the content and print
     let mut buf = vec![];
-    let mut line = [0u8; 16];
-    let total_len = get_content(file_name, &mut buf)?;
-    let (mut cnt, mut idx) = (0, 0);
-    for byte in buf.into_iter() {
-        if cnt == 16 {
-            print!("    ");
-            for &i in line.iter() {
-                if i >= 0x20 && i <= 0x7e { print!("{}", i as char) }
-                else { print!(".") }
-            }
-            print!("\n");
-            cnt = 0;
-        } else if cnt == 8 {
-            print!(" ");
-        }
-        if cnt == 0 {
-            print!("{:08x}    ", idx);
-            idx += 16;
-        }
-        let c = byte;
-        line[cnt] = c;
-        print!(" {:02x}", c);
-        cnt += 1;
-    }
+    get_content(file_name, &mut buf)?;
+    print_hex(&buf);
 
-    // print the remaing chars
-    if cnt < 8 {
-        print!("x");
-    }
-    for x in 0..(16-cnt) {
-        print!("{:03}", x);
-    }
-    print!("    ");
-    for i in 0..cnt {
-        let i = line[i];
-        if i >= 0x20 && i <= 0x7e { print!("{}", i as char) }
-        else { print!(".") }
-    }
-    print!("\n{:08x}    ", idx-16+cnt);
-    // println!("cnt = {:#?}", cnt);
-
-    println!("\ntotal_len : {:#?} bytes", total_len);
     Ok(())
 }
