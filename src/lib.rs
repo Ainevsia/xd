08203f57a1e12e46e2acbde91d3475acad198b47
Ainevsia <zhipengxu@sjtu.edu.cn>
2020-01-21 16:56:26 +0800

    colors length function finished

--- src/lib.rs
+++ src/lib.rs
@@ -4,17 +4,21 @@ use exitfailure::ExitFailure;
 use std::io::BufReader;
 use std::io::prelude::*;
 use std::fs::File;
+use colored::*;
 
 #[derive(Debug, StructOpt)]
-#[structopt(name = "xd", about = "An toy implementation of Linux command xxd to view the content of a file in Hex.")]
+#[structopt(
+    name = "xd",
+    about = "An toy implementation of Linux command xxd to view the content of a file in Hex.",
+    author = "Ainevsia")]
 pub struct Opt {
     #[structopt(short, long, help = "turn colorization on")]
     color_on: bool,
 
-    #[structopt(short, long, default_value = "128", help = "stop after <length> bytes")]
-    length: usize,
+    #[structopt(short, long, help = "stop after <length> bytes")]
+    length: Option<usize>,
 
-    #[structopt(parse(from_os_str))]
+    #[structopt(name = "filename", parse(from_os_str))]
     input_file: path::PathBuf,
 
 }
@@ -27,6 +31,14 @@ impl Opt {
     pub fn get_file_name(&self) -> &path::Path {
         &self.input_file
     }
+
+    pub fn get_length(&self) -> Option<usize> {
+        self.length
+    }
+
+    pub fn get_color_option(&self) -> bool {
+        self.color_on
+    }
 }
 
 pub fn get_content(file_name: &path::Path, buf: &mut Vec<u8>) -> Result<usize, ExitFailure>
@@ -38,9 +50,14 @@ pub fn get_content(file_name: &path::Path, buf: &mut Vec<u8>) -> Result<usize, E
     )
 }
 
-pub fn print_hex(buf: &Vec<u8>) {
+pub fn print_hex(buf: &Vec<u8>, len: Option<usize>) {
     let (mut cnt, mut idx) = (0, 0);
     let mut line = [0u8; 16];
+    let len = match len {
+        Some(len) => len,
+        None => std::usize::MAX,
+    };
+    let mut printed = 0;
 
     for &byte in buf.iter() {
         if cnt == 16 {  // one line of hex end, print corresponding chars
@@ -61,6 +78,8 @@ pub fn print_hex(buf: &Vec<u8>) {
         line[cnt] = byte;
         print!(" {:02x}", byte);
         cnt += 1;
+        printed += 1;
+        if printed >= len { break }
     }
 
     // print the remaing chars
@@ -76,5 +95,52 @@ pub fn print_hex(buf: &Vec<u8>) {
     // finish summary
     let length = idx - 16 + cnt;
     println!("\n{:08x}", length);
+    println!("total length : {:#?} bytes", length);
+}
+
+pub fn print_hex_color(buf: &Vec<u8>, len: Option<usize>) {
+    let (mut cnt, mut idx) = (0, 0);
+    let mut line = [0u8; 16];
+    let len = match len {
+        Some(len) => len,
+        None => std::usize::MAX,
+    };
+    let mut printed = 0;
+    for &byte in buf.iter() {
+        if cnt == 16 {  // one line of hex end, print corresponding chars
+            print!("    ");
+            for &i in line.iter() {
+                if i >= 0x20 && i <= 0x7e { print!("{}", format!("{}", i as char).blue()) }
+                else { print!("{}", ".".blue()) }
+            }
+            print!("\n");
+            cnt = 0;
+        } else if cnt == 8 {    // middle of the hex, add one blank space
+            print!(" ");
+        }
+        if cnt == 0 {   // start a new line
+            print!("{}", format!("{:08x}    ", idx).cyan());
+            idx += 16;
+        }
+        line[cnt] = byte;
+        print!("{}", format!(" {:02x}", byte).green());
+        cnt += 1;
+        printed += 1;
+        if printed >= len { break }
+    }
+
+    // print the remaing chars
+    if cnt < 8 { print!(" ") }
+    for _ in 0..16 - cnt { print!("   ") }
+    print!("    ");
+    for c in 0..cnt {
+        let c = line[c];
+        if c >= 0x20 && c <= 0x7e { print!("{}", format!("{}", c as char).blue()) }
+        else { print!("{}", ".".blue()) }
+    }
+
+    // finish summary
+    let length = idx - 16 + cnt;
+    println!("{}", format!("\n{:08x}", length).cyan());
     println!("total length : {:#?} bytes", length);
 }
--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,19 @@ use xd::*;
 fn main() -> Result<(), ExitFailure> {
     // phare the argument and figure out the filename
     let opt = Opt::new();
+    // println!("opt = {:#?}", opt);
+
     let file_name = opt.get_file_name();
     println!("File name : {:#?}", file_name);
 
     // get the content and print
     let mut buf = vec![];
     get_content(file_name, &mut buf)?;
-    print_hex(&buf);
+    match opt.get_color_option() {
+        true => print_hex_color(&buf, opt.get_length()),
+        false => print_hex(&buf, opt.get_length()),
+    }
+    
 
     Ok(())
 }
